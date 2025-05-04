686b2a58dabd41c011768df6fe2ec6a7eb55a346
zulqarnainali96 <zuhairbaig23@gmail.com>
2025-05-04 21:46:30 +0500

    third message

--- script.js
+++ script.js
@@ -13,9 +13,11 @@ class Calculator {
     this.screen2 = screen2;
   }
   printScreen(item) {
-    Calculator.previousValue = item.textContent;
     let value = item.textContent;
     this.screen2.value += value;
+    if (this.screen2.value > 0) {
+      Calculator.nextValue = this.screen2.value;
+    }
   }
   clearScreen() {
     this.screen1.value = "";
@@ -24,42 +26,45 @@ class Calculator {
   plusOperation(item) {
     this.screen1.value = this.screen2.value;
     this.screen2.value = "";
-    if (this.screen2.value?.length > 0) {
-      Calculator.nextValue = this.screen2.value;
-      this.screen2.value = item.textContent;
-    }
-    if (item.textContent === "=") {
-      console.log(`${Calculator.previousValue} + ${Calculator.nextValue}`);
-    }
-    // console.log(this.screen2.value);
-    // console.log(Calculator.previousValue);
+    Calculator.previousValue = this.screen1.value;
   }
   operations(item) {
-    console.log(item.textContent);
+    // console.log(parseInt(Calculator.previousValue) + parseInt(Calculator.nextValue))
+    if (Calculator.previousValue.includes("/")) {
+      Calculator.plus =
+        parseInt(Calculator.previousValue) / parseInt(Calculator.nextValue);
+    } else if (Calculator.previousValue.includes("+")) {
+      Calculator.plus =
+        parseInt(Calculator.previousValue) + parseInt(Calculator.nextValue);
+    } else if (Calculator.previousValue.includes("*")) {
+      Calculator.plus =
+        parseInt(Calculator.previousValue) * parseInt(Calculator.nextValue);
+    } else if (Calculator.previousValue.includes("-")) {
+      Calculator.plus =
+        parseInt(Calculator.previousValue) - parseInt(Calculator.nextValue);
+    }
+    this.screen1.value = Calculator.plus;
+    this.screen2.value = "";
   }
   minusOperation() {
     this.screen1.value = this.screen2.value;
     this.screen2.value = "";
-    if (this.screen2.value?.length > 0) {
-      this.screen2.value = item.textContent;
-    }
-    console.log(this.screen2.value);
+    Calculator.previousValue = this.screen1.value;
   }
   divideOperation() {
     this.screen1.value = this.screen2.value;
     this.screen2.value = "";
-    if (this.screen2.value?.length > 0) {
-      this.screen2.value = item.textContent;
-    }
-    console.log(this.screen2.value);
+    Calculator.previousValue = this.screen1.value;
   }
   multiplyOperation() {
     this.screen1.value = this.screen2.value;
     this.screen2.value = "";
+    Calculator.previousValue = this.screen1.value;
+  }
+  deleteBack() {
     if (this.screen2.value?.length > 0) {
-      this.screen2.value = item.textContent;
+      this.screen2.value = this.screen2.value?.slice(0, this.screen2.value?.length - 1);
     }
-    console.log(this.screen2.value);
   }
 }
 
@@ -72,7 +77,7 @@ const caculator = new Calculator(element, screen1, screen2);
 
 buttons.forEach((item) => {
   item.addEventListener("click", () => {
-    if (item.textContent != "=") {
+    if (item.textContent != "=" && item.textContent !== "Delete") {
       caculator.printScreen(item);
     }
     if (item.textContent == "Clear") {
@@ -92,6 +97,9 @@ buttons.forEach((item) => {
     }
     if (item.textContent == "=") {
       caculator.operations(item);
+    }
+    if (item.textContent == "Delete") {
+      caculator.deleteBack();
     }
   });
 });
